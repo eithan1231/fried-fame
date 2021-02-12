36543f97b0c4173602dc2d0f289a5c5bd2eec87e
Eithan <eithantuit10@gmail.com>
2021-02-13 05:03:24 +1100

    Maintenance scripts removal, needs rehaul

--- update.sh
+++
@@ -1,8 +0,0 @@
-php ./upgrade_database.php
-chown www-data:www-data -R ./
-
-# For whatever reason, the delivery via sendfile requires execute permissions on nix.
-chmod 500 -R ./
-chmod 700 -R ./logs
-chmod 700 -R ./errors
-chmod 700 -R ./library/uploads
